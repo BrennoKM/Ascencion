9d47b6e3f0105109c4838cf9c28f7be5b92edee3
Ryan Borges <ryanborgeslockser@gmail.com>
2023-05-02 16:26:02 -0300

    correção bug do monstro

--- src/Model/Entity/Game.java
+++ src/Model/Entity/Game.java
@@ -4,26 +4,49 @@ import java.io.IOException;
 import java.util.Random;
 import java.util.Scanner;
 
+import Model.DAO.UsuarioDAO;
 import Model.Entity.Enums.Naipe;
 import Model.Entity.Enums.Valor;
+import Model.Service.UsuarioService;
 
 public class Game {
 	public static Scanner scanner = new Scanner(System.in);
 	
 	public static void main(String []args) {
 		
-		/*Usuario usuario = new Usuario();
-		//String caminhoDoArquivo = "C:\\Users\\Usuário\\Documents\\GitHub\\Ascencion\\src\\Database\\usuarios.txt";
-		String caminhoDoArquivo = "C:\\Users\\Ryan\\eclipse-workspace-projects\\Ascencion\\src\\Database\\usuarios.txt";
-			Usuario[] usuarios;
-		    try {
-		        usuarios = usuario.lerUsuariosDoArquivo(caminhoDoArquivo);
-		        // fazer alguma coisa com o array de usuários
-		    } catch (IOException e) {
-		        e.printStackTrace();
+		/*
+		String caminho = "C:\\Users\\Usuário\\Documents\\GitHub\\Ascencion\\src\\Database\\usuarios.txt";
+		 Usuario[] usuarios = UsuarioDAO.lerUsuariosDoArquivo(caminho);
+		    for (Usuario usuario : usuarios) {
+		        System.out.println(usuario.toString());
 		    }
+		    
+		System.out.print("Digite o nome do usuario:");
+		String nome = scanner.next();
+		System.out.print("Digite a senha do usuario:");
+		String senha = scanner.next();
+		UsuarioDAO.cadastrarUsuario(caminho, nome, senha);
+		 
+		usuarios = UsuarioDAO.lerUsuariosDoArquivo(caminho);
+	    for (Usuario usuario : usuarios) {
+	        System.out.println(usuario.toString());
+	    }
+	    
+	    System.out.print("Digite o login: ");
+	    nome = scanner.next();
+	    System.out.print("Digite a senha: ");
+	    senha = scanner.next();
+	    Usuario jogador = UsuarioDAO.fazerLogin(caminho, nome, senha);
+
+	    if (jogador != null) {
+	        System.out.println("Login realizado com sucesso. Dados do jogador:");
+	        UsuarioService.exibirPainelDeUsuario(jogador);
+	    } else {
+	        System.out.println("Não foi possível fazer o login. Verifique as informações de login e senha.");
+	    }
 		*/
 		
+
 		Monstro monster = new Monstro();
 		monster.adicionarCartas();
 		monster.embaralhar();
@@ -35,10 +58,10 @@ public class Game {
 		
 		/*
 		baralho.limpar(); //Criação de um baralho aleatorio
-		for (int i = 0; i < 52; i++) {
+		for (int i = 0; i < 50; i++) {
 		    Carta carta = new Carta(Naipe.values()[new Random().nextInt(Naipe.values().length)], Valor.values()[new Random().nextInt(Valor.values().length)]);
 		    baralho.adicionarCarta(carta);
-		}	*/
+		} */
 		
 		 System.out.print("BEM-VINDO AO GAME ASCENSION\n\n");
 		 Jogador jogador = new Jogador(); // Jogador criado
@@ -108,8 +131,6 @@ public class Game {
 			}
 			jogador.setBaralhoDeClasse(baralhoJogador);
 			System.out.print(jogador.toString());
-	    
-
-	
+		
 	}
 }
